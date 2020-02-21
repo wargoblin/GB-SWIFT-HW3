5b0f8f9b2f232a7c02f221bc719f5f29351f4711
Vasilzakiev <zvasil@gmail.com>
2020-02-21 16:39:19 +0300

    оформил названия файлов

---
+++ 2l HW_Закиев Василь.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+var str = "Hello, playground"
+print (str)
+
+// задание 1
+
+var num = 48
+func evenOrNot {
+    if num % 2 == 0 {
+        return true }
+    else {
+        ruturn false
+    }
+    
+}
